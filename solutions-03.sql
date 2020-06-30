737f37faadb655577213dd140520a0cfd677c5b0
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-06-30 14:04:22 -0500

    Do challenge 3

---
+++ solutions-03.sql
@@ -0,0 +1,15 @@
+USE publications;
+SELECT	authors.au_id AS 'AUTHOR ID',
+		authors.au_lname AS 'LAST NAME',
+        authors.au_fname AS 'FIRST NAME',
+        SUM(sales.qty) AS 'TOTAL'
+FROM authors
+INNER JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN sales
+ON titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(sales.qty) DESC
+LIMIT 3;
