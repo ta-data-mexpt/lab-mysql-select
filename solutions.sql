e8fddcecc25ae4c8e13b08d7821862688ae05d86
Chema Ramos <josemaria.rug@gmail.com>
2020-10-29 21:01:28 -0600

    Agrego bonus

--- solutions.sql
+++ solutions.sql
@@ -63,20 +63,12 @@ ORDER BY Units_sold DESC;
 
 -- ----------------------------
 #BONUS CHALLENGE
-SELECT C.au_id, C.au_lname, C.au_fname, ROUND(SUM(C.Profit),2) as 'Total_profit'
-FROM
-(SELECT B.au_id, B.au_lname, B.au_fname, B.title, B.advance*B.Royalty_div+ (B.price*B.Units_sold*B.Royalty_pct*Royalty_div) as 'Profit'
-FROM
-(SELECT authors.au_id, authors.au_lname, authors.au_fname, titles.title, titles.advance, titles.price, SUM(sales.qty) AS 'Units_sold', titles.royalty/100 as 'Royalty_pct', titleauthor.royaltyper/100 AS 'Royalty_div'
+SELECT authors.au_id, authors.au_lname, authors.au_fname, ROUND(SUM(titleauthor.royaltyper/100* titles.advance + titles.royalty/100*titles.ytd_sales),2) AS 'Profit' 
 FROM publications.authors AS authors
 JOIN publications.titleauthor titleauthor
 ON authors.au_id = titleauthor.au_id
 LEFT JOIN publications.titles as titles
 ON titleauthor.title_id = titles.title_id
-JOIN publications.sales as sales
-ON titles.title_id = sales.title_id
-GROUP BY authors.au_id, titles.title, titles.advance, titles.royalty, titles.price, titleauthor.royaltyper
-ORDER BY titles.title) AS B) AS C
-GROUP BY C.au_id
-ORDER BY Total_profit DESC
+GROUP BY authors.au_id
+ORDER BY Profit DESC
 LIMIT 3;
