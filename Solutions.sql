cf865987e27bf56f2bc39ec799e6fb25939b5e73
nayiturbel <naiiturbe@hotmail.com>
2020-10-31 22:34:05 -0600

    Lab terminado

---
+++ Solutions.sql
@@ -0,0 +1,65 @@
+#Challenge 1
+#What titles each author has published at which publishers
+SELECT authors.au_id AS 'Author ID', authors.au_lname AS 'Last Name', authors.au_fname AS 'First Name', titles.title AS 'Title', publishers.pub_name AS 'Publisher'
+FROM authors
+JOIN titleauthor 
+ON authors.au_id = titleauthor.au_id
+JOIN titles 
+ON titles.title_id = titleauthor.title_id 
+JOIN publishers 
+ON publishers.pub_id  = titles.pub_id 
+GROUP BY authors.au_id, titles.title, publishers.pub_name
+ORDER BY authors.au_id;
+
+#Challenge 2
+# How many titles each author has published at each publisher
+SELECT authors.au_id AS 'Author ID', authors.au_lname AS 'Last Name', authors.au_fname AS 'First Name', titles.title AS 'Title', publishers.pub_name AS 'Publisher', count(titles.title) as 'Title Count'
+FROM authors
+JOIN titleauthor 
+ON authors.au_id = titleauthor.au_id 
+JOIN titles 
+ON titles.title_id = titleauthor.title_id
+JOIN publishers 
+ON publishers.pub_id  = titles.pub_id 
+GROUP BY  publishers.pub_name, authors.au_id, titles.title
+ORDER BY authors.au_id DESC;
+
+#Challenge 3
+#Top 3 authors who have sold the highest number of titles
+SELECT authors.au_id AS 'Author ID', authors.au_lname AS 'Last Name', authors.au_fname AS 'First Name', sum(sales.qty) AS 'Total'
+FROM authors 
+JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+JOIN titles
+ON titles.title_id = titleauthor.title_id 
+JOIN sales 
+ON titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY Total DESC
+LIMIT 3;
+
+#Challenge 4
+#Best selling 23 authors ranking
+SELECT authors.au_id AS 'Author ID', authors.au_lname AS 'Last Name', authors.au_fname AS 'First Name', sum(sales.qty) AS 'Total'
+FROM authors 
+JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+JOIN titles
+ON titles.title_id = titleauthor.title_id 
+JOIN sales 
+ON titles.title_id = sales.title_id
+GROUP BY authors.au_id
+ORDER BY Total DESC
+LIMIT 23;
+
+#Bonus challenge
+# 3 Most profiting authors
+SELECT authors.au_id AS 'Author ID', authors.au_lname AS 'Last Name', authors.au_fname AS 'First Name', SUM(titles.advance + titles.royalty) AS 'Profit'
+FROM authors
+JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+JOIN titles
+ON titles.title_id = titleauthor.title_id 
+GROUP BY authors.au_id
+ORDER BY Profit DESC
+LIMIT 3;
