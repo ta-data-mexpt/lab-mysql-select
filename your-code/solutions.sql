1c6b687ad43115c17d515d3ef9b7341c26e029de
StephanieCortes <step_0707@hotmail.com>
2020-11-03 18:23:41 -0600

    ejercicio completo

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -4,11 +4,27 @@ LEFT JOIN publications.titleauthor AS tit ON au.au_id=tit.au_id
 LEFT JOIN publications.titles AS ti ON tit.title_id=ti.title_id
 LEFT JOIN publications.publishers AS pub ON ti.pub_id=pub.pub_id;
 
-
-SELECT DISTINCT au.au_id, au.au_lname, au.au_fname, ti.title, pub.pub_name, COUNT(pub.pub_name) AS 'Title_count'
+SELECT a.au_id, a.au_lname, a.au_fname, a.title, a.pub_id
+FROM(
+SELECT au.au_id, au.au_lname, au.au_fname, ti.title, pub.pub_id, COUNT(au.au_id) AS 'Title_count'
 FROM publications.authors AS au
 LEFT JOIN publications.titleauthor AS tit ON au.au_id=tit.au_id
 LEFT JOIN publications.titles AS ti ON tit.title_id=ti.title_id
-LEFT JOIN publications.publishers AS pub ON ti.pub_id=pub.pub_id;
-GROUP BY au.au_id;
+LEFT JOIN publications.publishers AS pub ON ti.pub_id=pub.pub_id) AS a
+GROUP BY a.au_id;
+
+SELECT au.au_id, au.au_lname, au.au_fname, SUM(ti.title_id) AS 'Number_of_titles'
+FROM publications.authors AS au
+LEFT JOIN publications.titleauthor AS tit ON au.au_id = tit.au_id
+LEFT JOIN publications.titles AS ti ON tit.title_id=ti.title_id
+GROUP BY au.au_id 
+ORDER BY 'Number_of_titles' DESC LIMIT 3;
+
+SELECT au.au_id, au.au_lname, au.au_fname, SUM(ti.title_id) AS 'Number_of_titles'
+FROM publications.authors AS au
+LEFT JOIN publications.titleauthor AS tit ON au.au_id = tit.au_id
+LEFT JOIN publications.titles AS ti ON tit.title_id=ti.title_id
+GROUP BY au.au_id 
+ORDER BY 'Number_of_titles' DESC LIMIT 23;
+
 
