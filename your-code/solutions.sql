d2be976361a7e019176e0bccc528250ed4968284
Alejandro Flores <alejandro.efo@gmail.com>
2021-05-09 21:07:15 -0500

    ahora si a dormir

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -22,5 +22,27 @@ JOIN publishers p
 	ON t.pub_id = p.pub_id
 GROUP BY a.au_id
 ORDER BY au_id DESC;
-    
+
+CREATE TABLE CHALLENGE_3
+SELECT a.au_id, a.au_lname, a.au_fname, sum(t.ytd_sales) AS total
+FROM authors a
+JOIN titleauthor ta
+	ON a.au_id = ta.au_id
+JOIN titles t
+	ON ta.title_id = t.title_id
+GROUP BY a.au_id, a.au_lname, a.au_fname
+ORDER BY total DESC   
+LIMIT 3;
+
+CREATE TABLE CHALLENGE_4
+SELECT authors.au_id, authors.au_lname, authors.au_fname, IFNULL(SUM(sales.qty),0) AS 'TOTAL'
+FROM publications.authors
+JOIN publications.titleauthor
+	ON authors.au_id=titleauthor.au_id
+JOIN publications.titles
+	ON titleauthor.title_id=titles.title_id
+JOIN publications.sales
+	ON titles.title_id=sales.title_id
+GROUP BY publications.authors.au_id
+ORDER BY SUM(sales.qty) DESC;
  
